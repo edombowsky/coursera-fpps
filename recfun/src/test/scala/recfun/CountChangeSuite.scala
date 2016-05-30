abecaae614a45ad3e3303e849af03ef353c07053
Earl Dombowsky <Earl.Dombowsky@ca.abb.com>
2016-05-30 09:17:04 -0700

    completed counting change

--- recfun/src/test/scala/recfun/CountChangeSuite.scala
+++ recfun/src/test/scala/recfun/CountChangeSuite.scala
@@ -25,4 +25,12 @@ class CountChangeSuite extends FunSuite {
     assert(countChange(300,List(500,5,50,100,20,200,10)) === 1022)
   }
 
+  test("countChange: no money") {
+    assert(countChange(0,List(500,5,50,100,20,200,10)) === 1)
+  }
+
+  test("countChange: no coins") {
+    assert(countChange(100,List()) === 0)
+  }
+
 }
